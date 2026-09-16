b61a4913f7ac571ebfb4b5226cfde96ff63a777f
jeremiah chinedu <chinedujeremiah2002@gmail.com>
2026-09-16 13:52:59 +0100

    implement main application logic with configuration loading and logging setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,25 @@
-package api
+package main
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/callmhejerry/sms/internal/shared/config"
+	"github.com/callmhejerry/sms/internal/shared/logger"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+	cfg, err := config.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	log := logger.New(cfg.AppEnv)
+
+	log.Info("Starting application", slog.String("env", cfg.AppEnv), slog.String("port", cfg.AppPort))
+
+	fmt.Println("Database DSN:", cfg.DSN())
+}
---
+++ internal/shared/config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	AppEnv  string
+	AppPort string
+
+	DBHost     string
+	DBUser     string
+	DBName     string
+	DBPort     string
+	DBPassword string
+	DBSSLMode  string
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{
+		AppEnv:     getEnv("APP_ENV", "development"),
+		AppPort:    getEnv("APP_PORT", "8081"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBUser:     getEnv("DB_USER", "school"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBPassword: getEnv("DB_PASSWORD", "schoolsecret"),
+		DBName:     getEnv("DB_NAME", "school_sms"),
+		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+	}
+	if err := cfg.validate(); err != nil {
+		return nil, fmt.Errorf("Invalid config: %w", err)
+	}
+	return cfg, nil
+}
+
+func (cfg *Config) validate() error {
+	if cfg.DBUser == "" {
+		return fmt.Errorf("DB_USER is required")
+	}
+	if cfg.DBName == "" {
+		return fmt.Errorf("DB_NAME is required")
+	}
+	if cfg.DBPassword == "" {
+		return fmt.Errorf("DB_PASSWORD is required")
+	}
+	return nil
+}
+
+// DSN returns the PostgreSQL connection string.
+func (c *Config) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName, c.DBSSLMode,
+	)
+}
+
+func getEnv(key, defaultValue string) string {
+	value, found := os.LookupEnv(key)
+	if !found {
+		return defaultValue
+	}
+	return value
+}
+
+func getEnvInt(key string, defaultValue int) int {
+	valueStr, found := os.LookupEnv(key)
+	if !found {
+		return defaultValue
+	}
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultValue
+}
